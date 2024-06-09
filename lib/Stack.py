03b65fd9fbd8a2c4c1d14d9b0db6fbf9f462ead5
+ <+>
2024-06-09 22:38:12 +0300

    changes

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,43 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
+        self.items = []
+        self.limit = limit
+        for items in items:
+            if (not self.full()):
+                self.items.append(items)
         pass
 
     def isEmpty(self):
-        pass
+        return self.items == []
+    pass
 
     def push(self, item):
+        if (not self.full()):
+            self.items.append(item)
+        else:
+             return None
         pass
 
     def pop(self):
-        pass
+        if self.isEmpty():
+            return None
+        return self.items.pop()
 
     def peek(self):
-        pass
-    
+        return self.items[len(self.items)]
+
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+
+        if (len(self.items) >= self.limit):
+            return True
+        return False
 
     def search(self, target):
-        pass
+        for i in reversed(range(len(self.items))):
+            if self.items[i] == target:
+                return len(self.items) -1  - i 
+        return -1
